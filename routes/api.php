b939e87148cc9393203bec5c351be60ab218575d
Alexander Hartsuyev <sehenge@gmail.com>
2025-10-31 20:38:46 +0300

    Added webhooks

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\TelegramWebhookController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
