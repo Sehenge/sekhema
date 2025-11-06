5c7569c4c88229e2ebcf95dc643e88474f311733
Alexander Hartsuyev <sehenge@gmail.com>
2025-11-06 16:16:51 +0300

    fix:added setwebhook

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/telegram/webhook', [TelegramWebhookController::class, 'handle']);
-Route::post('/telegram/setWebhook', [TelegramWebhookController::class, 'setWebhook']);
+Route::get('/telegram/setWebhook', [TelegramWebhookController::class, 'setWebhook']);
