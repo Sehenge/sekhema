e3d19dddbea5796549ca6b3f9d37fe0ec79b04b7
Alexander Hartsuyev <sehenge@gmail.com>
2025-12-27 15:33:28 +0300

    fix:tests

--- app/Jobs/AskChatGptJob.php
+++ app/Jobs/AskChatGptJob.php
@@ -48,7 +48,7 @@ class AskChatGptJob implements ShouldQueue
                 ->connectTimeout(10)
                 ->retry(1, 2000)
                 ->post(config('openai.base_url').'/chat/completions', [
-                    'model' => 'gpt-3.5-turbo',
+                    'model' => 'gpt-4o',
                     'messages' => [
                         [
                             'role' => 'system',
