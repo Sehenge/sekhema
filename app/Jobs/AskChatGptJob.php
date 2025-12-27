0be43e6927fe9fc55449bbfe53f547255cd37735
Alexander Hartsuyev <sehenge@gmail.com>
2025-12-27 15:20:01 +0300

    fix:model changed

--- app/Jobs/AskChatGptJob.php
+++ app/Jobs/AskChatGptJob.php
@@ -46,7 +46,7 @@ class AskChatGptJob implements ShouldQueue
                 ->connectTimeout(10)
                 ->retry(3, 2000)
                 ->post(config('openai.base_url').'/chat/completions', [
-                    'model' => 'gpt-4o-mini',
+                    'model' => 'gpt-5-nano',
                     'messages' => [
                         [
                             'role' => 'system',
